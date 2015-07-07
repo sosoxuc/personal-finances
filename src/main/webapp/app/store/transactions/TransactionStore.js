50f2b479eb18d7052d5e373f724c08f0733fc21e
sosoxuc <soso_xuc@yahoo.com>
2015-07-08 01:06:19 +0400

    trasaction support

--- src/main/webapp/app/store/transactions/TransactionStore.js
+++ src/main/webapp/app/store/transactions/TransactionStore.js
@@ -1,24 +1,34 @@
 Ext.define("TR.store.transactions.TransactionStore", {
-	extend : "Ext.data.Store",
-	storeId : 'transactionsStore',
-	// leadingBufferZone : 150,
-	// sortable: false,
-	// defaultSortable: false,
-	// buffered : true,
-	// remoteSort: false,
-	// pageSize : 50,
-	fields : [ 'id', 'transactionAmount', {
-		name : 'transactionDate',
-		convert : function(v) {
-			return v ? new Date(v) : '';
-		}
-	}, 'debitAccountName', 'creditAccountName', 'projectId', 'projectName',
-			'reasonId', 'reasonName', 'note' ],
-	proxy : {
-		url : 'rest/transaction/search',
-		type : 'rest',
-		reader : {
-			type : 'json'
-		}
-	}
+    extend : "Ext.data.Store",
+    storeId : 'transactionsStore',
+    leadingBufferZone : 150,
+    sortable : false,
+    defaultSortable : false,
+    buffered : true,
+    remoteSort : false,
+    pageSize : 50,
+    fields : [
+        'id',
+        'transactionAmount',
+        'transactionRest', 
+        'transactionOrder',
+        {
+            name : 'transactionDate',
+            convert : function(v) {
+                return v ? new Date(v) : '';
+            }
+        }, {
+            name : 'userDate',
+            convert : function(v) {
+                return v ? new Date(v) : '';
+            }
+        },
+        'note' ],
+    proxy : {
+        url : 'rest/transaction/search',
+        type : 'rest',
+        reader : {
+            type : 'json'
+        }
+    }
 });
