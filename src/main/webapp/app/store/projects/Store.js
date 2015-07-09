794e9f0900ea9c45e4643b6017a8a4b0ec520ef3
sosoxuc <soso_xuc@yahoo.com>
2015-07-09 19:19:58 +0400

    projects support

--- src/main/webapp/app/store/projects/Store.js
+++ src/main/webapp/app/store/projects/Store.js
@@ -1,6 +1,7 @@
 Ext.define('TR.store.projects.Store', {
     extend : 'Ext.data.Store',
     storeId: 'projectsStore',
+    autoLoad: true,
     fields: ['id', 'projectName'],
     proxy: {
     	url: 'rest/project/list',
--- src/main/webapp/app/view/projects/AddWindow.js
+++ src/main/webapp/app/view/projects/AddWindow.js
@@ -6,104 +6,108 @@ Ext.define("TR.view.projects.AddWindow", {
     autoShow : true,
     layout : 'border',
     constructor : function(cfg) {
-	    cfg = cfg || {};
-	    var me = this;
+        cfg = cfg || {};
+        var me = this;
 
-	    me.title = cfg.edit ? 'რედაქტირება' : 'დამატება';
+        me.title = cfg.edit ? 'რედაქტირება' : 'დამატება';
+        var form = Ext.create('Ext.form.Panel', {
+            border: false,
+            split: true,
+            //width: 350,
+            region: 'center',
+            bodyPadding: 5,
+            fieldDefaults : {
+                labelWidth : 150,
+                labelAlign : 'right',
+                anchor : '100%',
+                allowBlank: false
+            },
+            defaultType : 'textfield',
+            items : [ {
+                fieldLabel : 'დასახელება',
+                name : 'projectName',
+                value: cfg.data ? cfg.data.projectName : ''
+            }]
+        });
+        
 
-	   
-	    
-	    var form = Ext.create('Ext.form.Panel', {
-	    	border: false,
-	    	split: true,
-	    	//width: 350,
-	    	region: 'center',
-	    	bodyPadding: 5,
-	        fieldDefaults : {
-	            labelWidth : 150,
-	            labelAlign : 'right',
-	            anchor : '100%',
-	            allowBlank: false
-	        },
-	        defaultType : 'textfield',
-	        items : [ {
-	            fieldLabel : 'დასახელება',
-	            name : 'projectName'
-	        }]
-	    });
-	    
+        me.items = [ form ];
 
-	    me.items = [ form ];
+        me.buttons = [{
+            text : cfg.edit ? 'რედაქტირება' : 'დამატება',
+            handler : cfg.edit ? edit : add
+        }];
+        
+        me.callParent(arguments);
 
-	    me.buttons = [{
-	        text : cfg.edit ? 'რედაქტირება' : 'დამატება',
-	        handler : cfg.edit ? edit : add
-	    }];
-	    
-	    me.callParent(arguments);
+        me.on({
+            show: function(formPanel, options) {
+                form.getForm().findField('projectName').focus(true, 10);
+            }
+        });
+        
+        function add() {
+            if (!form.getForm().isValid())
+                return;
+            var values = form.getForm().getValues();
+            
+            myRequest({
+                url : 'rest/project/create',
+                params : values,
+                callback : function(id) {
+                    cfg.grid.store.load();
+                    cfg.grid.getSelectionModel().deselectAll();
+                    me.close();
+                }
+            });
+        }
 
-	    function add() {
-		    if (!form.getForm().isValid())
-			    return;
-		    var values = form.getForm().getValues();
-		    
-		    myRequest({
-		        url : 'rest/project/create',
-		        params : values,
-		        callback : function(id) {
-		            cfg.grid.store.load();
-		            cfg.grid.getSelectionModel().deselectAll();
-		        	me.close();
-		        }
-		    });
-	    }
-
-	    function edit() {
-	    	if (!form.getForm().isValid())
-			    return;
-		    var values = form.getForm().getValues();
-		    myRequest({
-		        url : 'rest/project/update',
-		        params : values,
-		        callback : function(id) {
-			        cfg.searchForm.search();
-		        	me.close();
-		        }
-		    });
-	    }
-	    
-	    function addPosition(){
-	    	Ext.Msg.prompt("პოზიციის დამატება", "პოზიცია", function(ans, text){
-	    		if(ans == 'ok' && text){
-	    			myRequest({
-	    				url: 'rest/employee/addPosition',
-	    				params: {
-	    					name: text
-	    				},
-	    				callback: function(id){
-	    					if(id) positionStore.load();
-	    					positionCombo.setValue(id);
-	    				}
-	    			});
-	    		}
-	    	});
-	    }
-	    
-	    function addWorkplace(){
-	    	Ext.Msg.prompt("სამუშაო ადგილის დამატება", "სამუშაო ადგილი", function(ans, text){
-	    		if(ans == 'ok' && text){
-	    			myRequest({
-	    				url: 'rest/employee/addWorkplace',
-	    				params: {
-	    					name: text
-	    				},
-	    				callback: function(id){
-	    					if(id) workplaceStore.load();
-	    					workplaceCombo.setValue(id);
-	    				}
-	    			});
-	    		}
-	    	});
-	    }
+        function edit() {
+            if (!form.getForm().isValid())
+                return;
+            var values = form.getForm().getValues();
+            myRequest({
+                url : 'rest/project/update',
+                params : values,
+                callback : function(id) {
+                    cfg.searchForm.search();
+                    me.close();
+                }
+            });
+        }
+        
+        function addPosition(){
+            Ext.Msg.prompt("პოზიციის დამატება", "პოზიცია", function(ans, text){
+                if(ans == 'ok' && text){
+                    myRequest({
+                        url: 'rest/employee/addPosition',
+                        params: {
+                            name: text
+                        },
+                        callback: function(id){
+                            if(id) positionStore.load();
+                            positionCombo.setValue(id);
+                        }
+                    });
+                }
+            });
+        }
+        
+        function addWorkplace(){
+            Ext.Msg.prompt("სამუშაო ადგილის დამატება", "სამუშაო ადგილი", function(ans, text){
+                if(ans == 'ok' && text){
+                    myRequest({
+                        url: 'rest/employee/addWorkplace',
+                        params: {
+                            name: text
+                        },
+                        callback: function(id){
+                            if(id) workplaceStore.load();
+                            workplaceCombo.setValue(id);
+                        }
+                    });
+                }
+            });
+        }
     }
 });
