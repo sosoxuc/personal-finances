27cf354c64a601094029f4d0061570d765eecc03
sosoxuc <soso_xuc@yahoo.com>
2015-07-18 10:40:06 +0400

    account tests

--- src/test/java/personal/finances/accounts/AccountServiceTest.java
+++ src/test/java/personal/finances/accounts/AccountServiceTest.java
@@ -1,13 +1,68 @@
 package personal.finances.accounts;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import personal.spring.SpringDevConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles({ "dev" })
+@ContextConfiguration(classes = { SpringDevConfig.class })
+@WebAppConfiguration
 public class AccountServiceTest {
 
+    @Autowired
+    private WebApplicationContext wac;
+
     @Test
-    public void test() {
+    public void testAccounts() throws Exception {
+        MockMvc mock = MockMvcBuilders.webAppContextSetup(wac).build();
+        ResultActions result;
+        result = mock
+                .perform(post("/account/create").param("accountName", "test"));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$").exists());
+        String jsonStr = result.andReturn().getResponse().getContentAsString();
+        Account account = new ObjectMapper().readValue(jsonStr, Account.class);
+        String idStr = account.id.toString();
+        Integer id = account.id;
+
+        result = mock.perform(get("/account/list"));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$[0].id").value(id));
+        result.andExpect(jsonPath("$[0].accountName").value("test"));
+
+        result = mock.perform(post("/account/update").param("id", idStr)
+                .param("accountName", "test2"));
+        result.andExpect(status().isOk());
+
+        result = mock.perform(get("/account/list"));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$[0].id").value(id));
+        result.andExpect(jsonPath("$[0].accountName").value("test2"));
+
+        result = mock.perform(post("/account/remove").param("id", idStr));
+        result.andExpect(status().isOk());
+
+        result = mock.perform(get("/account/list"));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$[0]").doesNotExist());
     }
 
 }
