5d296160f7e8511d3cb7a2ae1b27cc791f3672cc
sosoxuc <soso_xuc@yahoo.com>
2015-07-25 19:45:16 +0400

    transaction recalculate button

--- src/main/webapp/app/view/rests/MainPanel.js
+++ src/main/webapp/app/view/rests/MainPanel.js
@@ -13,26 +13,37 @@ Ext.define("TR.view.rests.MainPanel", {
         var panel = Ext.create('Ext.panel.Panel',{
             defaultType : 'textfield',
             title : 'მიმდინარე ნაშთები',
+            buttonAlign: 'left',
+            buttons: [{
+                text : 'განახლება',
+                handler : load
+            }]
         });
         
         me.items = [ panel ];
         
         me.callParent(arguments);
         
-        myRequest({
-            url : 'rest/transaction/rests/currencies',
-            method : 'GET',
-            callback : function(data){
-                Ext.Array.forEach(data, function(item){
-                    panel.add({
-                        fieldLabel : item.resourceName,
-                        labelAlign : 'right',
-                        labelWidth : 150,
-                        value: item.transactionRest
+        load();
+        
+        function load() {
+            myRequest({
+                url : 'rest/transaction/rests/currencies',
+                method : 'GET',
+                callback : function(data){
+                    Ext.Array.forEach(data, function(item){
+                        panel.removeAll();
+                        panel.add({
+                            fieldLabel : item.resourceName,
+                            labelAlign : 'right',
+                            labelWidth : 150,
+                            value: item.transactionRest,
+                            readOnly: true
+                        })
                     })
-                })
-                
-            }
-        });
+                    
+                }
+            });
+        }
     }
 });
