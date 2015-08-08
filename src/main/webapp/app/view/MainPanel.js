01e3942a5eaac534a9a2a848189640abc041a0d6
sosoxuc <soso_xuc@yahoo.com>
2015-08-08 16:01:57 +0400

    hr

--- src/main/webapp/app/view/MainPanel.js
+++ src/main/webapp/app/view/MainPanel.js
@@ -38,7 +38,7 @@ Ext.define('TR.view.MainPanel', {
             handler : logout
         } ];
 
-        //var employees = Ext.create('TR.view.employees.MainPanel');
+        var employees = Ext.create('TR.view.employees.MainPanel');
         var transactions = Ext.create('TR.view.transactions.MainPanel');
         var projects = Ext.create('TR.view.projects.MainPanel');
         var rests = Ext.create('TR.view.rests.MainPanel');
