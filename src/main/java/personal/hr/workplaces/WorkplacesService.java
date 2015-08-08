a96ccb7547579a50a2137652d24c1a7f9e052dd2
Niko <nikolozgrdzelidze@gmail.com>
2015-08-08 16:01:09 +0400

    workpace remove

--- src/main/java/personal/hr/workplaces/WorkplacesService.java
+++ src/main/java/personal/hr/workplaces/WorkplacesService.java
@@ -5,12 +5,15 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import personal.States;
+import sun.security.provider.certpath.OCSPResponse;
 
 @RestController
 @RequestMapping("/hr/workplace")
@@ -20,19 +23,32 @@ public class WorkplacesService {
     private EntityManager em;
 
     @RequestMapping("/workplace/list")
-    public List<Workplace> getWorkplaces() {
-        return em.createQuery("from Workplace", Workplace.class)
+    public ResponseEntity<List<Workplace>> getWorkplaces() {
+        List<Workplace> workplaces = em.createQuery("from Workplace", Workplace.class)
                 .getResultList();
+
+        return new ResponseEntity<>(workplaces, HttpStatus.OK);
     }
 
     @RequestMapping("/workplace/add")
     @Transactional(rollbackFor = Throwable.class)
-    public Integer addWorkplace(@RequestParam String name) {
+    public ResponseEntity<Workplace> addWorkplace(@RequestParam String name) {
         Workplace workplace = new Workplace();
         workplace.workplaceName = name;
         workplace.stateId=States.ACTIVE;
         em.persist(workplace);
 
-        return workplace.id;
+        return new ResponseEntity<>(workplace, HttpStatus.OK);
     }
+
+    @RequestMapping("/workplace/remove")
+    @Transactional(rollbackFor = Throwable.class)
+    public ResponseEntity<Workplace> removeWorkplace(@RequestParam Integer id) {
+        Workplace workplace = em.find(Workplace.class, id);
+        workplace.stateId=States.INACTIVE;
+
+        return new ResponseEntity<>(workplace, HttpStatus.OK);
+    }
+
+
 }
